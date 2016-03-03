a8c7a500f56cce90287f6cf937c78571d3c43ba6
U-ARRS\ilagiglia <ilagiglia@lp-ilagiglia.ARRS.ARRISI.com>
2016-03-03 17:02:07 -0300

    connecting to db

--- src/github.com/pos/infrastructure/db.go
+++ src/github.com/pos/infrastructure/db.go
@@ -1,11 +1,26 @@
 package infrastructure
 
 import (
-	"github.com/pos/domain/item"
+	"github.com/pos/domain"
+	"database/sql"
+	_"github.com/go-sql-driver/mysql"
+	"fmt"
 )
-type Db interface {
 
-	Save(item.Item)
-	Get(string)
+type CatalogDB struct {
+	db sql.DB
 }
 
+func (catDb *CatalogDB) init() {
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		panic(fmt.Sprintf("Error when opening database connection: %v", err))
+	}
+	catDb.db = db
+}
+
+func (* CatalogDB) GetItem(id int64) (domain.Item){
+
+
+	return nil
+}
--- src/github.com/pos/service/service.go
+++ src/github.com/pos/service/service.go
@@ -6,20 +6,24 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/pos/domain/item"
+
+	"github.com/pos/domain"
+	"github.com/pos/infrastructure"
 )
 
 type Service struct {
 	name string
+	db infrastructure.CatalogDB
 }
 
-
+//GET catalog/prod/{id}
 func (service Service) HandleProducts(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
 	prodId := vars["id"]
 
-	item :=  item.New(prodId)
+	item :=  domain.NewItem(prodId)
 
-
+	service.db.GetItem(item.ñ)
 
 	fmt.Fprintf(w, "ProductId: %q %q %s", service.name, html.EscapeString(r.URL.Path), prodId)
 }
