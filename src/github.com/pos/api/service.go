01acc7cf00e6a7c39186cf5f7e6a81e359a89294
U-ARRS\ilagiglia <ilagiglia@lp-ilagiglia.ARRS.ARRISI.com>
2016-03-14 17:16:04 -0300

    adding more and more UTs

--- src/github.com/pos/api/service.go
+++ src/github.com/pos/api/service.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"os"
+
 	"fmt"
 	//"html"
 	"net/http"
@@ -10,10 +10,14 @@ import (
 	"encoding/json"
 	"github.com/pos/dto"
 	"io/ioutil"
-	log "github.com/Sirupsen/logrus"
+	//log "github.com/Sirupsen/logrus"
+	"log"
 )
 
+type GetPathParams func (*http.Request) map[string]string
+
 type Service struct {
+	GetRequestParameters GetPathParams
 	error string
 	name string
 	db infrastructure.DB
@@ -23,15 +27,16 @@ type Service struct {
 func NewService(db infrastructure.DB) *Service{
 
 	// Log as JSON instead of the default ASCII formatter.
-	log.SetFormatter(&log.JSONFormatter{})
+	//log.SetFormatter(&log.JSONFormatter{})
 
 	// Output to stderr instead of stdout, could also be a file.
-	log.SetOutput(os.Stderr)
+	//log.SetOutput(os.Stderr)
 
 	// Only log the warning severity or above.
-	log.SetLevel(log.DebugLevel)
+	//log.SetLevel(log.DebugLevel)
 
 	service := new(Service)
+	service.GetRequestParameters = getPathParams
 	service.header_handler = make(map[string] func(http.ResponseWriter,*http.Request))
 	service.db = db
 	service.error = "ERROR"
@@ -47,7 +52,8 @@ func (service Service) HandleError(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "The request method is not supported for the requested resource")
 }
 
-//GET or PUT over /catalog/products/{id}
+//Handle request of type GET and PUT against /catalog/products/{id}
+//This method derives to another different handler according to the HTTP method.
 func (service Service) HandleRequest(w http.ResponseWriter, r *http.Request){
 
 	handler := service.header_handler[r.Method]
@@ -61,41 +67,46 @@ func (service Service) HandleRequest(w http.ResponseWriter, r *http.Request){
 //URL catalog/products/{id}
 func (service Service) HandleGetItem(w http.ResponseWriter, r *http.Request) {
 
-	vars := mux.Vars(r)
+	vars := service.GetRequestParameters(r)
+
 	prodId := vars["id"]
 	item := service.GetItem(prodId)
 
 	if item.Id == "" {
 		w.WriteHeader(http.StatusNotFound)
+		log.Printf("GET item_id: %s not found", prodId)
 		return
 	}
 
 	strB, _ := json.Marshal(item)
 
 	fmt.Fprintf(w, "%s", strB)
-	log.WithFields(log.Fields{"itemid":item.Id, "descripton":item.Desc, "price":item.Price}).Debugf("GET item:")
+	log.Printf("GET item_id: %s returned OK", item.Id)
 
 }
 //PUT catalog/products/{id}
 func (service Service) HandlePutItem(w http.ResponseWriter, r *http.Request){
+	vars := service.GetRequestParameters(r)
+	item_id := vars["id"]
 
 	body, _ := ioutil.ReadAll(r.Body)
 
-	item := dto.Item{}
+	item := new(dto.Item)
 
-	if err := json.Unmarshal(body, &item); err != nil {
+	if err := json.Unmarshal(body, item); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, "The request contains a wrong format: %s ", err)
+		log.Printf("PUT item_id: %s .The request contains a wrong format %s", item.Id, err)
 		return
 	}
-
-	fmt.Fprintf(w, "%s", item)
-
+	item.Id = item_id
+	service.PutItem(item.Id, item.Desc, item.Price)
+	w.WriteHeader(http.StatusCreated)
 
 }
 
 func (service Service) GetItem(id string) dto.Item {
-	log.WithFields(log.Fields{"itemid":id}).Debugf("Get item:")
+	log.Printf("Getting item_id: %s from DB", id)
 	item := service.db.GetItem(id)
 	return  item;
 }
@@ -104,7 +115,15 @@ func (service Service) PutItem(id string, desc string, price float32) dto.Item {
 
 	itemDto := dto.Item{id, desc, price};
 	service.db.SaveItem(itemDto)
-	log.WithFields(log.Fields{"itemid":id, "descripton": desc, "price": price}).Debugf("Saving item:")
+	log.Printf("PUT item_id: %s returned OK", itemDto.Id)
 
 	return itemDto
 }
+
+//This function returns a map containing all the path params contained in the request URL.
+//In this case, the implementation uses mux.
+//This function is used by default, but can be overwritten for testing purposes or any other one.
+func getPathParams(r *http.Request) map[string]string {
+	return mux.Vars(r)
+}
+
--- src/github.com/pos/api/service_test.go
+++ src/github.com/pos/api/service_test.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"log"
-	//"io/ioutil"
+	"strings"
+	"bytes"
+	"fmt"
 )
 
 //DB MOCK
@@ -16,24 +18,22 @@ func init() {
 	db = infrastructure.NewMemDb()
 }
 
-//TESTS
-
+//TESTs to check GET /catalog/products/{id} results
 func Test_returns_404_when_item_does_not_exist(t *testing.T){
 
 	service := NewService(db)
 	ts := httptest.NewServer(http.HandlerFunc(service.HandleGetItem))
 	defer ts.Close()
 
-	url := ts.URL + "/catalog/products/2";
+	item_id := "2"
+	url := ts.URL + "/catalog/products/" + item_id;
 
 	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if res.StatusCode != http.StatusNotFound {
+	if (err != nil) || (res.StatusCode != http.StatusNotFound) {
+		Log("GET", url, res.StatusCode, http.StatusOK)
 		t.Fail()
 	}
+
 }
 
 func Test_returns_200_when_item_exists(t *testing.T){
@@ -45,23 +45,99 @@ func Test_returns_200_when_item_exists(t *testing.T){
 	service := NewService(db)
 	service.PutItem(item_id, item_desc , item_price)
 
+	f := func (r *http.Request) map[string]string {
+		req_vars := make(map[string]string)
+		req_vars["id"]= item_id
+		log.Printf("Returning itemid:%s from a testing callback.", req_vars["id"])
+		return req_vars
+	}
+	service.GetRequestParameters = f
+
 	ts := httptest.NewServer(http.HandlerFunc(service.HandleGetItem))
 	defer ts.Close()
 
 	url := ts.URL + "/catalog/products/" + item_id;
-
 	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("GET URL: %s StatusCode different from what was expected: %d <> %d", url, res.StatusCode, http.StatusOK)
+	if (err != nil) || (res.StatusCode != http.StatusOK)  {
+		Log("GET", url, res.StatusCode, http.StatusOK)
 		t.Fail()
 	}
 }
 
-func Test_return_an_error_when_itemid_does_NOT_exist (t *testing.T) {
+//TESTs to check PUT /catalog/products/{id} results
+func Test_returns_201_when_item_is_created (t *testing.T) {
+
+	item_id := "123133131"
+	item_desc := "item for testing purposes"
+	item_price :=  "2.0"
+
+	service := NewService(db)
+
+	ts := httptest.NewServer(http.HandlerFunc(service.HandlePutItem))
+	defer ts.Close()
+
+	url := ts.URL + "/catalog/products/" + item_id;
+	body_as_string := "{\"description\":\"" + item_desc + "\", \"price\":" + item_price + "}"
+	body := strings.NewReader(body_as_string)
+	req,err := http.NewRequest("PUT", url, body)
+	if err != nil {
+		log.Printf("Error when creating PUT request %d.", err)
+		t.Fail()
+	}
+
+	res, err := http.DefaultClient.Do(req)
+
+
+	if (err != nil) || (res.StatusCode != http.StatusCreated)  {
+		Log("PUT", url, res.StatusCode, http.StatusCreated)
+		t.Fail()
+	}
+}
+
+func Test_returns_the_item_after_it_is_created(t *testing.T){
+	item_id := "112"
+	item_desc := "item for testing purposes"
+	item_price :=  "2.0"
+
+	service := NewService(db)
+
+	f := func (r *http.Request) map[string]string {
+		req_vars := make(map[string]string)
+		req_vars["id"]= item_id
+		log.Printf("Returning itemid:%s from a testing callback.", req_vars["id"])
+		return req_vars
+	}
+	service.GetRequestParameters = f
+
+	tsPUT := httptest.NewServer(http.HandlerFunc(service.HandlePutItem))
+	defer tsPUT.Close()
+
+	url := tsPUT.URL + "/catalog/products/" + item_id;
+	body_as_string := "{\"description\":\"" + item_desc + "\", \"price\":" + item_price + "}"
+	body := strings.NewReader(body_as_string)
+	req,err := http.NewRequest("PUT", url, body)
+	if err != nil {
+		log.Printf("Error when creating PUT request %d.", err)
+		t.Fail()
+	}
+	res, err := http.DefaultClient.Do(req)
+
+	if (err != nil) || (res.StatusCode != http.StatusCreated)  {
+		Log("PUT", url, res.StatusCode, http.StatusCreated)
+		t.Fail()
+	}
+
+	tsGET := httptest.NewServer(http.HandlerFunc(service.HandleGetItem))
+	defer tsGET.Close()
+	url = tsGET.URL + "/catalog/products/" + item_id;
+	res, err = http.Get(url)
+	if (err != nil) || (res.StatusCode != http.StatusOK)  {
+		Log("GET", url, res.StatusCode, http.StatusOK)
+		t.Fail()
+	}
+}
+
+func Test_returns_an_error_when_item_does_NOT_exist (t *testing.T) {
 
 	service := NewService(db);
 	item := service.GetItem("1021")
@@ -71,7 +147,7 @@ func Test_return_an_error_when_itemid_does_NOT_exist (t *testing.T) {
 	}
 }
 
-func Test_return_an_itemid_just_saved (t *testing.T) {
+func Test_return_an_item_just_saved (t *testing.T) {
 
 	id := "2"
 	price := float32(10)
@@ -95,4 +171,12 @@ func Test_return_an_empty_item_if_it_does_not_exist(t *testing.T){
 	if item.Id != "" {
 		t.Fail()
 	}
+}
+
+func Log(method string, url string, expectedStatusCode int, receivedStatusCode int){
+
+	var buf bytes.Buffer
+	logger := log.New(&buf, "logger: ", log.Lshortfile)
+	logger.Printf("%s URL: %s StatusCode %d different from what was expected %d", method, url, expectedStatusCode, receivedStatusCode)
+	fmt.Print(&buf)
 }
