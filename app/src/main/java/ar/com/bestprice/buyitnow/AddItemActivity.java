e9c521ae8665962470d5f06be9b215be26604846
Ivan Lagiglia <lagigliaivan@gmail.com>
2016-03-22 18:49:55 -0300

    First commit of android client application

---
+++ app/src/main/java/ar/com/bestprice/buyitnow/AddItemActivity.java
@@ -0,0 +1,18 @@
+package ar.com.bestprice.buyitnow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by ilagiglia on 22/03/2016.
+ */
+public class AddItemActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+}
+
---
+++ app/src/main/java/ar/com/bestprice/buyitnow/MainActivity.java
@@ -0,0 +1,167 @@
+package ar.com.bestprice.buyitnow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        setContentView(R.layout.activity_main);
+        ListView view =  (ListView) findViewById(R.id.item_list_view);
+
+        FrameLayout footerLayout = (FrameLayout) getLayoutInflater().inflate(R.layout.footer, null);
+        //Button btnPostYourEnquiry = (Button) footerLayout.findViewById(R.id.button);
+
+        view.addHeaderView(footerLayout);
+
+
+        ArrayAdapter<String> mForecastAdapter =
+                new ArrayAdapter<String>(
+                        view.getContext(), // The current context (this activity)
+                        R.layout.activity_main, // The name of the layout ID.
+                        R.id.item_text_view, // The ID of the textview to populate.
+                        weekForecast);
+
+        view.setAdapter(mForecastAdapter);
+
+    }
+
+
+    private void connect(){
+
+        // These two need to be declared outside the try/catch
+        // so that they can be closed in the finally block.
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        // Will contain the raw JSON response as a string.
+        String forecastJsonStr = null;
+
+        try {
+            // Construct the URL for the OpenWeatherMap query
+            // Possible parameters are avaiable at OWM's forecast API page, at
+            // http://openweathermap.org/API#forecast
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+            // Create the request to OpenWeatherMap, and open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // Read the input stream into a String
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return ;
+            }
+            forecastJsonStr = buffer.toString();
+        } catch (IOException e) {
+            Log.e("PlaceholderFragment", "Error ", e);
+            // If the code didn't successfully get the weather data, there's no point in attemping
+            // to parse it.
+            return;
+        } finally{
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+
+        return;
+    }
+
+    public void onClick(View view) {
+        System.out.println(((TextView)view).getText());
+    }
+
+    public void onClickButton(View view){
+        Intent intent = new Intent(this, AddItemActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.gridLayout);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+
+
+}
